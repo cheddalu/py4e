95cb0b729e88575ebc4eec36c8138fe585683275
chedda <chedda@cheddas-MacBook-Pro.local>
2018-02-28 08:30:48 -0800

    2-4.py completed

---
+++ 2-4.py
@@ -0,0 +1,19 @@
+# assign the 2 variables, 1 int and 1 float value
+width = 17
+height = 12.0
+# width//2
+# width/2.0
+# height/3
+# 1 + 2 * 5
+
+print('width//2')
+print(width//2)
+
+print('width/2.0')
+print(width/2.0)
+
+print('height/3')
+print(height/3)
+
+print('1 + 2 * 5')
+print(1 + 2 * 5)
